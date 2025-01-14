948bbee4f8194a4c3faa5002c073cf4ef5ea9ddf
Jose Storopoli <jose@storopoli.com>
2025-01-14 15:40:18 -0300

    doc: update descriptor table in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -5,14 +5,13 @@
 //! The format is designed to be compact
 //! and efficiently represent standard Bitcoin output types:
 //!
-//! | Type | Payload Len | Payload Interpretation |
-//! | ---- | ----------- | ---------------------- |
-//! | 0    | 0..=80      | `OP_RETURN` payload    |
-//! | 1    | 20          | P2PKH hash             |
-//! | 2    | 20          | P2SH hash              |
-//! | 3    | 20          | P2WPKH hash            |
-//! | 3    | 32          | P2WSH hash             |
-//! | 4    | 32          | P2TR X-only PubKey     |
+//! | Type | Payload Length(s) | Payload Interpretation | Spend Type    | Mainnet Address Prefix |
+//! | ---- | ----------------- | ---------------------- | ------------- | ---------------------- |
+//! | 0    | ..=80             | `OP_RETURN` payload    | (N/A)         | (N/A)                  |
+//! | 1    | 20                | pubkey hash            | P2PKH         | `1...`                 |
+//! | 2    | 20                | script hash            | P2SH          | `3...`                 |
+//! | 3    | 20, 32            | SegWit v0 hash         | P2WPKH, P2WSH | `bc1q...`              |
+//! | 4    | 32                | SegWit v1 public key   | P2TR          | `bc1p...`              |
 
 #[cfg(feature = "address")]
 pub mod address;

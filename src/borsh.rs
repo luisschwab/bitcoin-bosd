ea24818628a2d13d9cbb9c0605a3444a0644dce4
Luis Schwab <luisschwab@protonmail.com>
2025-10-25 23:11:40 -0400

    feat(borsh): add P2A support

--- src/borsh.rs
+++ src/borsh.rs
@@ -148,4 +148,19 @@ mod tests {
         assert_eq!(original, deserialized);
         assert_eq!(deserialized.type_tag(), DescriptorType::P2tr);
     }
+
+    #[test]
+    fn borsh_p2a() {
+        // P2A
+        // Using 0x05 (type_tag)
+        // Source: transaction c054743f0f3ecfac2cf08c40c7dd36fcb38928cf8e07d179693ca2692d041848
+        // Corresponds to address `bc1pfeessrawgf`
+        let original = Descriptor::from_str("05").unwrap();
+
+        let serialized = to_vec(&original).unwrap();
+        let deserialized = Descriptor::try_from_slice(&serialized).unwrap();
+
+        assert_eq!(original, deserialized);
+        assert_eq!(original.type_tag(), DescriptorType::P2a);
+    }
 }
